1a6a69b476c43d8e72e18b43d8a93cf1f9f82843
Paola Pintos <pgpcuy@gmail.com>
2024-09-21 20:45:36 -0300

    2- Varios cambios creacion front-end

---
+++ proyecto/usuarios/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=50)
+    texto = models.CharField(max_length=500)
+    # img= models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.titulo
+
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    user = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    post_id = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.nombre}, {self.apellido}"
