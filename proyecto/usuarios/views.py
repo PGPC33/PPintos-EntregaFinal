59a8628059474cf456e259e9e14590441133262c
Paola Pintos <pgpcuy@gmail.com>
2024-09-22 15:51:23 -0300

    feat(usuarios): listado de posts

--- proyecto/usuarios/views.py
+++ proyecto/usuarios/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post, Usuario
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    contexto = {"posts": posts}
+    return render(request, "usuarios/post_list.html", contexto)
