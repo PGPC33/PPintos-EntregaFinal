17bd45e47e8afd9f7bd8324836243d198073bbff
Paola Pintos <pgpcuy@gmail.com>
2024-09-22 16:20:03 -0300

    3- Se crea el listado de usuarios

--- proyecto/usuarios/urls.py
+++ proyecto/usuarios/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("post/list", views.post_list),
+    path("usuario/list", views.usuario_list),
 ]
--- proyecto/usuarios/views.py
+++ proyecto/usuarios/views.py
@@ -6,3 +6,9 @@ def post_list(request):
     posts = Post.objects.all()
     contexto = {"posts": posts}
     return render(request, "usuarios/post_list.html", contexto)
+
+
+def usuario_list(request):
+    usuarios = Usuario.objects.all()
+    contexto = {"usuarios": usuarios}
+    return render(request, "usuarios/usuario_list.html", contexto)
